fe29d38f5cdd2e250ae7f6dfa9d05526c211c611
aobo25800 <377598262@qq.com>
2020-09-11 18:51:17 +0800

    “创建springboot启动文件”

---
+++ src/main/java/com/example/springMybatisDemo/SprintMybatisApplication.java
@@ -0,0 +1,11 @@
+package com.example.springMybatisDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintMybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SprintMybatisApplication.class, args);
+    }
+}
